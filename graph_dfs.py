94908662448c7478f8f3581ca7c9e2e1a597bfb8
Harbour Bridge <maleb44@yahoo.com>
2016-01-15 11:04:46 +0100

    Graph sortest way
    Graph legrovidebb ut

---
+++ graph_dfs.py
@@ -0,0 +1,18 @@
+## Simple deepth first search
+
+
+
+def DFS(graph, start, end, path = [],shortest = None):
+# Assumes graph is a Digraph
+# Assumes start and end are nodes in graph
+    path = path + [start]
+    print 'Current dfs path:', printPath(path)
+    if start == end:
+        return path
+    for node in graph.childrenOf(start):
+        if node not in path: # Avoid cycles
+            if shortest == None or len(path) < len(shortest):
+                newPath = DFS(graph, node, end, path, shortest)
+                if newPath != None:
+                    shortest = newPath
+    return shortest
